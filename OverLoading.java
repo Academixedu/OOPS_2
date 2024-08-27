230915f129dc3bd924e5a563d68e01338cf273bc
Manohar624 <manoharnaidu4703@gmail.com>
2024-08-27 19:42:41 +0530

    1

--- OverLoading.java
+++ OverLoading.java
@@ -1,3 +1,17 @@
 public class OverLoading{
-// Prepare an Example to show OverLoading Concept
+    public int add(int a, int b) {
+    return a+b;
+    }
+        public int add(int a, int b, int c) {
+            return a+b+c;
+    }
+    public double add(double a, double b){
+        return a+b;
+    }
+    
+        public static void main(String[]args) {
+            System.out.println("sum of 5,10");
+            System.out.println("sum of 20,30,40");
+            System.out.println("sum of 2.5,4.5");
+    }
 }
