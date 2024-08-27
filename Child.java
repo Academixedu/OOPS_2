c035eccd45cda7aff96faa27337c99b1640fecb5
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-27 17:11:11 +0530

    done

--- Child.java
+++ Child.java
@@ -1,3 +1,15 @@
-public class Child{
+public class Child extends Parent{
+
+public String name1 (String name ){
+    System.out.println(name);
+ return name ;
+
+}
+ public static void main(String[] args) {
+    Child oo=new Child();
+    oo.name1("rohit");
+    oo.papa(10, 20);
+
 // Prepare Some Functions in Child Class and Extend it to Parent Class
+}
 }
--- Child1.java
+++ Child1.java
@@ -1,3 +1,13 @@
-public class Child1{
+public class Child1 extends Parent{
+
+public String name2(String name){
+    System.out.println(name);
+    return name;
+}
+public static void main(String[] args) {
+    Child1 hh=new Child1();
+    hh.name2("mohit");
+    hh.papa(90, 78);
+}
 // Prepare Some Function and extend to Parent Class
 }
