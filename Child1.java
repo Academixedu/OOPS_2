ea6a232ea51055d28673fc651648ccd2bd9d27ff
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-27 18:36:22 +0530

    shahid

--- Child1.java
+++ Child1.java
@@ -1,3 +1,13 @@
-public class Child1{
+public class Child1 extends Parent{
 // Prepare Some Function and extend to Parent Class
+public double treat(double b){
+    return b;
+}
+ public static void main(String[] args) {
+Child1 y=new Child1();
+System.out.println(y.dude("bye")+""+y.mr(00));
+    
+//     double y=1.2;
+}
+
 }
