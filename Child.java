c75e87e323cc73f270decfa87b88c08046629cdb
karthhiik <karthikthatikonda18@gmail.com>
2024-08-27 18:34:53 +0530

    Update Child.java

--- Child.java
+++ Child.java
@@ -1,3 +1,11 @@
-public class Child{
-// Prepare Some Functions in Child Class and Extend it to Parent Class
+public class child extends parent {
+    // Prepare Some Functions in Child Class and Extend it to Parent Class
+    public void sonname(String name) {
+        System.out.println("Son Name: " + name + " " + getName("T"));
+    }
+
+    public static void main(String[] args) {
+        child ch = new child();
+        ch.sonname("Karthik");
+    }
 }
