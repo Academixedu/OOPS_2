5c88dc9c54e91464611233793b8238709d193fb4
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-27 19:09:01 +0530

    Lavanya

--- OverLoading.java
+++ OverLoading.java
@@ -1,3 +1,24 @@
 public class OverLoading{
 // Prepare an Example to show OverLoading Concept
+
+
+public void dog(){
+    System.out.println(" dog ");
+}
+public void dog(String name){
+    System.out.println(" dog name " + name);
+}
+public void dog(String name, String color){
+    System.out.println(" dog name: " + name + " dog color: " +color);
+}
+public void dog(String name, String color, double cost ){
+    System.out.println(" dog name: " + name + " dog color: " + color + " dog cost: " + cost);
+}
+public static void main(String[] args){
+    OverLoading  d=new OverLoading();
+    d.dog();
+    d.dog("rikki");
+    d.dog("rikki", "black");
+    d.dog("rikki", "black",2500);
+}
 }
--- Parent.java
+++ Parent.java
@@ -1,4 +1,11 @@
-public class Parent{
+public class Parent {
 // Prepare a Function Here 
-  
+public String carname(){
+   String carname="";
+   return carname;
+}
+public String carcolor(){
+    String carcolor="";
+    return carcolor;
+ }
 }
